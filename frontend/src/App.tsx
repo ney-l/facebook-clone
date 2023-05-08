e048e4fa9bf4cd095c1470a6e22cd52204def6ef
Neha Demirci <neylanjewar@gmail.com>
2023-05-08 16:10:43 +0200

    feat: 🛣 Add path aliasing to projec

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { Sample } from '@/components/Sample';
+
 function App() {
   return (
     <>
       <div>
         <h1>Facebook clone</h1>
       </div>
+      <Sample />
     </>
   );
 }
